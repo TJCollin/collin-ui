7f4750a41acc9ac13075d36d390bba9aa4e9828e
collin <tjzhangcong@outlook.com>
2021-05-29 11:52:35 +0800

    export cardContent & SubMenu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,13 +1,12 @@
-import { FC } from "react";
-import Menu, { MenuProps } from "./menu";
-import MenuItem, { MenuItemProps } from "./menuItem";
-import SubMenu, { SubMenuProps } from "./subMenu";
+import Menu from "./menu";
+// import MenuItem, { MenuItemProps } from "./menuItem";
+// import SubMenu, { SubMenuProps } from "./subMenu";
 
-type MenuType = FC<MenuProps> & {
-  MenuItem: FC<MenuItemProps>;
-  SubMenu: FC<SubMenuProps>;
-};
-const MenuComponent = Menu as MenuType;
-MenuComponent.MenuItem = MenuItem;
-MenuComponent.SubMenu = SubMenu;
-export default MenuComponent;
+// type MenuType = FC<MenuProps> & {
+//   MenuItem: FC<MenuItemProps>;
+//   SubMenu: FC<SubMenuProps>;
+// };
+// const MenuComponent = Menu as MenuType;
+// MenuComponent.MenuItem = MenuItem;
+// MenuComponent.SubMenu = SubMenu;
+export default Menu;
