6e87e2937c5b8ba7e74f94467c619bc9524c9025
collin <tjzhangcong@outlook.com>
2021-06-17 21:40:41 +0800

    add forward ref for progress bar

--- src/components/ProgressBar/progressBar.tsx
+++ src/components/ProgressBar/progressBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { ThemeType } from "../Icon/icon";
 import ClassNames from "classnames";
 
@@ -16,31 +16,40 @@ export interface ProgressBarProps extends React.HTMLAttributes<HTMLDivElement> {
  * import {ProgressBar} from "collin-ui"
  *
  */
-export const ProgressBar: React.FC<ProgressBarProps> = (props) => {
-  const { theme, percent, className, style, height = 16, ...restProps } = props;
-  const styleWithHeight: React.CSSProperties = {
-    borderRadius: height / 2,
-    ...style,
-    height: `${height}px`,
-  };
-  const innerStyle: React.CSSProperties = {
-    width: `${percent}%`,
-    height: "100%",
-    borderRadius: height / 2,
-  };
-  const classes = ClassNames(
-    "progress-bar",
-    {
-      [`progress-${theme}`]: theme,
-    },
-    className
-  );
-  return (
-    <div style={styleWithHeight} {...restProps} className={classes}>
-      <div className="progress-bar-inner" style={innerStyle}></div>
-    </div>
-  );
-};
+export const ProgressBar = forwardRef<HTMLDivElement, ProgressBarProps>(
+  (props, ref) => {
+    const {
+      theme,
+      percent,
+      className,
+      style,
+      height = 16,
+      ...restProps
+    } = props;
+    const styleWithHeight: React.CSSProperties = {
+      borderRadius: height / 2,
+      ...style,
+      height: `${height}px`,
+    };
+    const innerStyle: React.CSSProperties = {
+      width: `${percent}%`,
+      height: "100%",
+      borderRadius: height / 2,
+    };
+    const classes = ClassNames(
+      "progress-bar",
+      {
+        [`progress-${theme}`]: theme,
+      },
+      className
+    );
+    return (
+      <div style={styleWithHeight} {...restProps} className={classes} ref={ref}>
+        <div className="progress-bar-inner" style={innerStyle}></div>
+      </div>
+    );
+  }
+);
 
 ProgressBar.defaultProps = {
   height: 15,
